c6f6dab31df4f18cabc8ee9814cffe99ae463b1b
Tim Porritt <tim.m.porritt@gmail.com>
2021-07-27 01:08:52 +1000

    Fix eslint not actually working

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,16 @@
 module.exports = {
-  extends: ["prettier"],
-  plugins: ["prettier"],
-  rules: {
-    "prettier/prettier": "error",
-    "arrow-body-style": "off",
-    "prefer-arrow-callback": "off",
-  },
+    root: true,
+    parser: '@typescript-eslint/parser',
+    extends: [
+        'prettier',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+    ],
+    plugins: ['@typescript-eslint', 'prettier'],
+    rules: {
+        'prettier/prettier': 'error',
+        'arrow-body-style': 'off',
+        'prefer-arrow-callback': 'off',
+        'dot-notation': 'warn',
+    },
 };
